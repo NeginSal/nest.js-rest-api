2dcbef913cb90de962c6ef23429737dac4fb0f5c
NeginSal <neginsaljooghi@gmail.com>
2025-01-27 15:23:11 +0330

    add findAll items

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'get all items';
+  }
+}
