9e6ca96ae1d00d9f8b69dd0db407b9b44e95f0ed
NeginSal <neginsaljooghi@gmail.com>
2025-02-01 16:06:13 +0330

    feat:add GET endpoint to get one item

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
@@ -7,9 +7,19 @@ export class ItemsController {
   findAll(): string {
     return 'get all items';
   }
- 
+
+  // @Get(':id')
+  // findOne(@Param() param): string {
+  //   return `Item ${param.id}`;
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
   @Post()
-  create( @Body() createItemDto:CreateItemDto): string {
+  create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name} Desc:${createItemDto.description}`
   }
 }
